e70a857a811b41b55465a69fb68892500aad4afd
janeth <jarri023@fiu.edu>
2019-12-10 20:32:29 -0500

    api

--- src/main/java/com/apprenticeevaluator/api/xcontroller/ApprenticeController.java
+++ src/main/java/com/apprenticeevaluator/api/xcontroller/ApprenticeController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/")
 public class ApprenticeController {
     ApprenticeService apprenticeService;
